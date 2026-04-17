24bf925b1badd7d0cd27a6911181236fa44dd087
JTM77XY <jeantales773@gmail.com>
2026-04-16 22:21:28 -0300

    alteração

---
+++ 01-javascript/textes.js
@@ -0,0 +1,26 @@
+// alert("olá mundo!")
+// alert("segunda mensagem")
+
+let nome
+
+// nome = "nomizinho aqui"
+
+// alert("nome:" + nome)
+
+// let idade = 30
+// alert("idade:" + idade)
+// alert("nome:" + nome + "\nidade: " + idade)
+
+
+// nome = prompt("digite seu nome:")
+// alert("nome digitado:" + nome)
+
+let nota1
+nota1 = prompt("digite a primeira nota:")
+nota1 = Number(nota1)
+nota1 = nota1 + 1
+alert(nota1)
+
+
+    
+
