b022e565c0755314b4e215e3bcd7275e0891104e
JTM77XY <jeantales773@gmail.com>
2026-04-23 20:33:10 -0300

    alterações

--- 01-javascript/textes.js
+++ 01-javascript/textes.js
@@ -1,26 +1,35 @@
 // alert("olá mundo!")
 // alert("segunda mensagem")
 
-let nome
+//let nome
+//let idade 
+// nome = prompt("digite o nome")
+ //idade = prompt("digite a idade")
+ //alert("nome:" + nome + "\nidade: " + idade)
 
-// nome = "nomizinho aqui"
-
-// alert("nome:" + nome)
-
-// let idade = 30
-// alert("idade:" + idade)
-// alert("nome:" + nome + "\nidade: " + idade)
-
-
+//let nome
 // nome = prompt("digite seu nome:")
 // alert("nome digitado:" + nome)
 
-let nota1
-nota1 = prompt("digite a primeira nota:")
-nota1 = Number(nota1)
-nota1 = nota1 + 1
-alert(nota1)
+//let nota1
+//nota1 = prompt("digite a primeira nota:")
+//nota1 = Number(nota1)
+//nota1 = nota1 + 1
+//alert(nota1)
+
+let vitorias, empates
+let pontos
+vitorias = prompt("vitórias:")
+vitorias = Number(vitorias)
+
+empates = prompt("empates:")
+empates = Number(empates)
+// empates = Number(prompt("empates"))
+
+pontos = vitorias*3 + empates
+ alert("total de pontos:" + pontos)
+
 
 
     
-
+/
