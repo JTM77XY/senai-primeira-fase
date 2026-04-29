831e17e3a3d45d11a33113a78830bdee2fa1eaf7
JTM77XY <jeantales773@gmail.com>
2026-04-29 14:49:53 -0300

    atualização

---
+++ 01-javascript/churrasco.js
@@ -0,0 +1,16 @@
+let pessoas, carne = 0.5, cerveja = 1, agua = 0.5, refri = 0.2
+let totalcarne, totalcereja, totalagua, totalrefri
+
+pessoas = Number(prompt("informe a quantidade de pessoas:"))
+
+totalcarne = pessoas*carne
+console. log(totalcarne.toFixed(2) + "kg")
+
+totalcereja = pessoas*cerveja
+console. log(totalcereja.toFixed(2) + "l")
+
+totalagua = pessoas*agua
+console. log(totalagua.toFixed(2) + "l")
+
+totalrefri = pessoas*refri
+console. log(totalrefri.toFixed(2) + "l")
---
+++ 01-javascript/precoracao.js
@@ -0,0 +1,7 @@
+let peso
+let preco
+
+peso = Number(prompt(" digite o peso da ração em g:"))
+
+preco = peso / 100
+console. log( "preço da ração é: " + "R$" + preco.toFixed(2))
