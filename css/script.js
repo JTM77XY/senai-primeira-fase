2a85c3c1fdf50948d4ec4253b8793fd20bcaf72e
JTM77XY <jeantales773@gmail.com>
2026-05-03 17:03:53 -0300

    atualizações

--- css/script.js
+++ css/script.js
@@ -62,6 +62,87 @@ function deslocamentoparaeventos(){
 
 }
 
+function entradanopalco(){
+let somfuncionando, figurinocompleto
+
+somfuncionando = (prompt("o sistema de som está funcionando? sim ou não:"))
+figurinocompleto = (prompt("o figurino está completo? sim ou não:"))
+
+if( somfuncionando == "sim" && figurinocompleto == "sim"){
+    document.getElementById("resultado").innerHTML = "Palco liberado para Sarumano! Luzes, câmera, ilusão!"
+}else{
+    if(somfuncionando == "não" && figurinocompleto == "sim"){
+        document.getElementById("resultado").innerHTML = "Sem som. Realizar apresentação alternativa no salão."
+    }else{
+        if(somfuncionando == "sim" && figurinocompleto == "não"){
+            document.getElementById("resultado").innerHTML = "Faltando parte do figurino. Te vira no improviso!"
+        }else{
+            document.getElementById("resultado").innerHTML = "Apresentação cancelada. Hora da mágica de desaparecer…"
+        }
+    }
+}
+
+}
+
+function SistemadeTransporte(){
+let emManutencao, emergenciaAtiva
+
+emManutencao = (prompt("ha manutenção no sistema da transporte? sim ou não:"))
+emergenciaAtiva = (prompt(" o nível de emergência do parque está alto? sim ou não"))
+
+if(emManutencao == "não" && emergenciaAtiva == "não"){
+    document.getElementById("resultado").innerHTML = "Transporte liberado para uso."
+}else{
+  document.getElementById("resultado").innerHTML = "Transporte indisponível por motivo de segurança." 
+}
+
+}
+
+function centrodecomando(){
+let crachaValido, digitalReconhecida
+
+crachaValido = Number(prompt("seu crachá está validado? digite o número do crachá:"))
+digitalReconhecida = (prompt("a digital está cadastrada? sim ou não:"))
+
+if(crachaValido == 1 && digitalReconhecida == "sim"){
+    document.getElementById("resultado").innerHTML = "Acesso liberado ao Centro de Comando"
+}else{
+  if(crachaValido != 1 && digitalReconhecida == "sim"){
+    document.getElementById("resultado").innerHTML =  "Crachá inválido. Dirija-se à recepção"
+  }else{
+    if(crachaValido == 1 && digitalReconhecida == "não"){
+    document.getElementById("resultado").innerHTML =  "Falha na digital. Tente novamente ou chame o suporte"
+    }else{
+        document.getElementById("resultado").innerHTML = "Acesso negado. Verifique sua identificação."
+    }
+  }  
+}
+
+}
+
+function jaulasautomatizadas(){
+let visitantesNaArea, horaAtual
+
+visitantesNaArea = Number(prompt("informe o número de visitantes na área:"))
+horaAtual = Number(prompt("informe o horário:"))
+
+if(visitantesNaArea == 0 && horaAtual > 18){
+  document.getElementById("resultado").innerHTML = "Liberação autorizada. Abrindo jaula." 
+}else{
+  if(visitantesNaArea == 0 && horaAtual <8){
+    document.getElementById("resultado").innerHTML = "Liberação autorizada. Abrindo jaula."
+  }else{
+    document.getElementById("resultado").innerHTML = "Liberação negada. Área em uso ou fora do horário permitido."
+  }
+   
+}
+
+}
+
+
+
+
+
 
 
 
