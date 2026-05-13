09d3483196cb872f32eff52262c9fbe885284acb
JEAN TALES MENEZES DE JESUS <jean_t_jesus@estudante.sesisenai.org.br>
2026-05-12 22:16:02 -0300

    atualizações

--- 01-javascript/jares.js
+++ 01-javascript/jares.js
@@ -1,7 +1,14 @@
-let numerocaminhoes, jarescaminhao=50, caminhaocobranca=450, jare=90
+let numerocaminhoes, jarescaminhao=50, caminhaocobranca=450, jare=90, NcaminhaoTemporadaP
 let lucro
 
 numerocaminhoes = Number(prompt("informe o número de caminhões:"))
+NcaminhaoTemporadaP = Number(prompt("quantos caminhões de jarés foram vendidos na temporada passada:"))
 
 lucro = numerocaminhoes*jarescaminhao*jare-numerocaminhoes*caminhaocobranca
 console. log("lucro da temporada é: " + "R$" + lucro)
+
+if(numerocaminhoes > NcaminhaoTemporadaP){
+    console.log("quantidade de caminhões aumentou.")
+}else if(NcaminhaoTemporadaP > numerocaminhoes){
+    console.log("quantidade de caminhões diminuiu.")
+}else{console.log("quantidade de caminhões se manteve a mesma.")}
--- 01-javascript/pesodosveiculos.js
+++ 01-javascript/pesodosveiculos.js
@@ -6,3 +6,10 @@ taradoveiculo = Number(prompt("informe somente o peso do veiculo"))
 
 cargakg = pesobrutototal - taradoveiculo
 console. log("o peso da carga que o caminhão está carregando é " + cargakg + "kg")
+
+if(pesobrutototal > 22000){
+    console.log("o peso bruto total excedeu o limite.")
+}
+if(cargakg > 14000){
+    console.log("peso liquido excedeu o limite")
+}
