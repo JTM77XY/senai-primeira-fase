9de059d67742dbcf09a3eecca184623c64cbdcd4
JEAN TALES MENEZES DE JESUS <jean_t_jesus@estudante.sesisenai.org.br>
2026-05-14 22:18:55 -0300

    atualização

---
+++ variaveis/script.js
@@ -0,0 +1,32 @@
+let cont = 0
+function incrementarContagem(){
+    
+    cont = cont + 1
+    document.getElementById("p-contagem").innerHTML = cont
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+let global = "eu sou uma variável global"
+function funcao1(){
+    let local = "sou uma variavel local :D"
+    console.log(local)
+    console.log(global)
+}
+function funcao2(){
+   console.log(global)
+//    console.log(local)
+}
+
+// funcao1()
+// funcao2()
