f373cfe50de4bdeebd562b35635e69774594c783
JEAN TALES MENEZES DE JESUS <jean_t_jesus@estudante.sesisenai.org.br>
2026-05-18 22:20:35 -0300

    atualização

--- variaveis/script.js
+++ variaveis/script.js
@@ -1,32 +1,68 @@
 let cont = 0
 function incrementarContagem(){
-    
-    cont = cont + 1
+    // cont = cont + 1
+    // cont+=
+    cont++
     document.getElementById("p-contagem").innerHTML = cont
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-let global = "eu sou uma variável global"
-function funcao1(){
-    let local = "sou uma variavel local :D"
-    console.log(local)
-    console.log(global)
+function decrementarcontagem(){
+    cont--
+    document.getElementById("p-contagem").innerHTML = cont
 }
-function funcao2(){
-   console.log(global)
+
+function somar10(){
+    cont = cont + 10
+    // cont += 10
+    document.getElementById('p-contagem').innerHTML = cont
+}
+
+function somar2(){
+    cont += 2
+    document.getElementById('p-contagem').innerHTML = cont
+}
+
+function zerarContagem(){
+    cont = 0
+    document.getElementById('p-contagem').innerHTML = cont
+}
+
+let total = 0
+function registrarVenda(){
+let valor = Number(document.getElementById("inputvenda").value)
+
+total = total + valor
+
+document.getElementById("totalvendido").innerHTML = 'Total de vendas: R$' + total.toFixed(2)
+document.getElementById("listavendas").innerHTML += 'R$' + valor.toFixed(2) + '<br>'
+document.getElementById("inputvendas").value = ''
+document.getElementById("inputvendas").focus()
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let global = "eu sou uma variável global"
+// function funcao1(){
+    // let local = "sou uma variavel local :D"
+    // console.log(local)
+    // console.log(global)
+// }
+// function funcao2(){
+//    console.log(global)
 //    console.log(local)
-}
+// }
 
 // funcao1()
 // funcao2()
