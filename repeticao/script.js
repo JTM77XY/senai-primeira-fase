e2817c5a75b4fcffbca8f4fcdd77a7a4144c1435
JEAN TALES MENEZES DE JESUS <jean_t_jesus@estudante.sesisenai.org.br>
2026-06-16 22:13:10 -0300

    atualização

--- repeticao/script.js
+++ repeticao/script.js
@@ -1,18 +1,76 @@
 // let contador = 0 // inicializacao
-
 // while(contador < 10){ //(condicao de repeticao)
 //     alert("contador: " + contador)                                     //while=enquanto
     
 //     contador++ // contagem
 // }//voltar e testar novemente
 
+// let peso 
+// let contador = 0
+// while(contador < 10){
+// peso = Number(prompt("digite do peso:"))
+// if(peso > 1.1 || peso < 0.9){
+//     alert("fora tolerância do peso")
+// }
+// contador++
+// }
 
-let peso 
-let contador = 0
-while(contador < 10){
-peso = Number(prompt("digite do peso:"))
-if(peso > 1.1 || peso < 0.9){
-    alert("fora tolerância do peso")
-}
-contador++
-}
+// let total = 0
+// let i = 0
+// while(i < 3){
+//     i++
+//     let preco = Number(prompt("digite o preço do " + i + "° produto:"))
+//     total = total + preco
+//     document.getElementById("total").innerHTML += i + "°produto: R$" + preco.toFixed(2)+"<br>"
+//     // i++
+// }
+// alert(total)
+
+
+//exercicio 1
+// let contadorqudrado = 0
+// while(contadorqudrado < 10){
+//     contadorqudrado++
+//     let quadrado = contadorqudrado*contadorqudrado
+//     console.log(contadorqudrado + ":" + quadrado)
+// }
+
+//exercicio 2
+// let contador = 10
+// while(contador >= 1){
+//     console.log(contador)
+//     contador--
+// }
+
+//exercicio 3    
+// let contador = 0
+// let soma = 0
+// while(contador < 100) {
+//    contador++
+//    soma = soma + contador
+//    console.log(soma)
+// }
+
+//exercicio 4
+// let resultado
+// let contador = 0
+// while(contador <= 10){
+//     resultado = 5*contador
+//     contador++
+//     console.log(resultado)
+// }
+
+//exercicio 6
+// let i = 0
+// while(i < 20){
+//     i++
+//     if(i%2 != 0){
+//     console.log(i)
+//     }
+    
+// }
+
+
+
+// i%3 == 0
+// i%5 == 0
