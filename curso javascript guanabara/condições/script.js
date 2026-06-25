d7425e41f4c69bfa760b9fdad0dde4da72c06187
JTM77XY <jeantales773@gmail.com>
2026-06-25 17:01:27 -0300

    atualização

---
+++ curso javascript guanabara/condições/script.js
@@ -0,0 +1,17 @@
+function calcular(){
+let txtv = Number(document.getElementById('txtvel').value)
+document.getElementById('res').innerHTML = `sua velocidade atual é de <strong>${txtv}km/h</strong>`
+if(txtv > 60){
+ document.getElementById('res').innerHTML += `<p>você está <strong>multado</strong> por excesso de velocidade</p>`   
+}
+document.getElementById('res').innerHTML += `<p>dirija com cinto de segurança!</p>`
+}
+
+
+/*
+esplicação sobre as saidas/respostas:
+as respostas ficam dentro de crazes e a variavel TXTV fica dentro das chaves e
+um elemento com ID recebe todas as respostas.
+as respostas são adicionadas uma depois da outra usando o operador += , que
+junta um elemento com outro elemento existente.
+*/
---
+++ curso javascript guanabara/condições/script2.js
@@ -0,0 +1,24 @@
+// condição simples
+// console.log(`a velocidade do seu carro é ${vel}km/h`)
+// let vel = 12
+// if(vel > 60){
+//     console.log(`você ultrapassou a velocidade permitida. multado!`)
+// }
+
+// condição composta
+// let país = 'EUA'
+// if(país == 'brasil'){
+//     console.log('brasileiro')
+// }else{
+//     console.log('estrangeiro')
+// }
+
+// condição aninhada
+// let idade = 22
+// if(idade < 16){
+//     console.log('não vota')
+// }else if(idade < 18 || idade > 65){
+//     console.log('voto opcional')
+// }else{
+//     console.log('voto obrigatório')
+// }
