35ce27e040ed095699452648bf7b6058a422560c
JEAN TALES MENEZES DE JESUS <jean_t_jesus@estudante.sesisenai.org.br>
2026-06-25 22:07:18 -0300

    atualização

--- desafio de sprint/script.js
+++ desafio de sprint/script.js
@@ -6,7 +6,9 @@ for(let i = 1; i <= sprint; i++){
 let tarefas = Number(prompt('quantas tarefas foram feitas diariamente'))
  total = total + tarefas
 
-  console.log( 'dia ' + i +': ' + tarefas + " tarefa(s) concluida(s)")
+  // console.log( 'dia ' + i +': ' + tarefas + " tarefa(s) concluida(s)")
+document.getElementById("texto").innerHTML += ` dia ${i}:${tarefas} tarefas concluida(s).`
 }
-console.log("total de tarefas concluidas: " + total)
+// console.log("total de tarefas concluidas: " + total)
+document.getElementById("texto").innerHTML += ` total de tarefas concluidas ${total}`
 }
