1468f6d21d071c53816aa892a0107d02143f5a7f
JEAN TALES MENEZES DE JESUS <jean_t_jesus@estudante.sesisenai.org.br>
2026-07-02 22:22:25 -0300

    atualização

--- desafio de sprint 2 bug/script.js
+++ desafio de sprint 2 bug/script.js
@@ -1,24 +1,36 @@
 function relatorio(){
-    let sprint = Number(prompt('quantos dias teve a sprint?'))
-    let total = 0
-    let media
-    let ListaBug = []
-    
+    let sprint = Number(prompt('quantos dias teve a sprint?')) 
+    let total = 0 // variavel para somar os bugs.
+    let media // variavel para o calculo da media dos bugs.
+    let ListaBug = [] // mostra uma lista com os bugs que foram informados em cada dia. serve só para mostrar os bugs separadamente em uma lista.
+
     for(let i = 1; i <= sprint; i++){
-      let bugs = Number(prompt(`quantos bugs foram encontrados no dia ${i}?`))
+     let bugs = Number(prompt(`quantos bugs foram encontrados no dia ${i}?`))
      ListaBug.push(bugs)
      total = total + bugs
      document.getElementById('bug').innerHTML += ` dia ${i}: ${bugs} bug(s).`
     }
 
-    let maior = Math.max(...ListaBug)
-    let menor = Math.min(...ListaBug)
+    let maior = Math.max(...ListaBug) //mostra o maior número dentre os números da ListaBug.
+    let menor = Math.min(...ListaBug) //mostra o menor número dentre os números da ListaBug.
+    let diamaior, diamenor;
+
+    diamaior = ListaBug.indexOf(maior) + 1 // pega a posição do maior número da ListaBug.
+    diamenor = ListaBug.indexOf(menor) + 1 // pega a posição do menor número da ListaBug.
+
     document.getElementById('bug').innerHTML += ` total de bugs: ${total}.`
     media = total/sprint
     document.getElementById('bug').innerHTML += ` media de bugs: ${media}.`
     document.getElementById('bug').innerHTML += ` maior quantidade de bugs: ${maior}.`
-    document.getElementById('bug').innerHTML += ` menor quantidade de bugs: ${menor}.`     
+    document.getElementById('bug').innerHTML += ` menor quantidade de bugs: ${menor}.`
+    
+    console.log('dia com menos bugs: '+ diamenor)
+    console.log( 'dia com mais bugs: '+diamaior)     
 }
+
+
+
+//indexOf()
 
 //criar duas variaveis número menor e número maior.
 // criar um if dentro do for.
---
+++ lista de exercícios de array/script.js
@@ -0,0 +1,36 @@
+//exercício 01
+// let semana = ['segunda','terça','quarta','quinta','sexta','sábado','domingo']
+// console.log(semana)
+
+//exercício 03
+// let semana = ['segunda','terça','quarta','quinta','sexta','sábado','domingo']
+// semana.push('feriado')
+// console.log(semana)
+
+//exercício 04
+// let semana = ['segunda','terça','quarta','quinta','sexta','sábado','domingo']
+// semana.pop()
+// console.log(semana)
+
+//exercício 05
+// let numerospares = [2,4,6,8,10,12,14,16,18,20]
+// console.log(numerospares)
+
+//exercício 06
+// let numerospares = [2,4,6,8,10,12,14,16,18,20]
+// let total = numerospares[2] = 12
+// console.log(numerospares)
+
+//exercício 07
+// let frutas = ['banana','maçã','uva']
+// frutas.push('morango')
+// console.log(frutas)
+
+//exercício 08
+let frutas = ['banana','uva','morango']
+
+    frutas.splice(1,1)
+    console.log(frutas)
+
+
+
