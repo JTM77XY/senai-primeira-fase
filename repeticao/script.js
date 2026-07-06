b12226de35680eff3f7b7145145978100a976f9d
JTM77XY <jeantales773@gmail.com>
2026-07-06 17:01:10 -0300

    atualização

--- repeticao/script.js
+++ repeticao/script.js
@@ -164,14 +164,14 @@
 // }
 
 //exer 04
-let numero = Number(prompt('informe um número'))
+// let numero = Number(prompt('informe um número'))
 
-for(let i = numero; i > 0 ; i--){ 
-   if(i % 2 != 0){
-   console.log(i+'é impar')
-  }
+// for(let i = numero; i > 0 ; i--){ 
+//    if(i % 2 != 0){   //vai testar se a divisão vai dar resto igual a 1. se o i for número impar, vai passar na condição se não for, não passa.
+//    console.log(i+'é impar')
+//   }
 
-}
+// }
 
 //exer 05
 // let asteriscos = ''
@@ -179,3 +179,127 @@ for(let i = numero; i > 0 ; i--){
 //   asteriscos += '*'
 //   console.log(asteriscos)
 // }
+
+//exer 06
+// let numerosorteado = Math.floor(Math.random() * 100)
+// console.log(numerosorteado)
+// let tentativas = 0
+// let botao = document.getElementById('clique')
+// botao.addEventListener("click", () => {
+//     tentativas++
+//     console.log(tentativas)
+// })
+
+// function sorteio(){
+//  let palpite = Number(document.getElementById('valor').value)
+//  if(palpite == numerosorteado){
+//   document.getElementById('total').innerHTML = `voçe acertou o número sorteado ${numerosorteado}`
+//  }else if(palpite > numerosorteado){
+//   document.getElementById('total').innerHTML = `o número sorteado é menor que ${palpite}`
+//  }else if(palpite < numerosorteado){
+//   // console.log
+//     document.getElementById('total').innerHTML = `o número sorteado é maior que ${palpite}`
+//  }
+
+// }
+
+
+// // O computador sorteia um número de 1 a 100
+// const numeroSecreto = Math.floor(Math.random() * 100) + 1;
+// let tentativas = 0;
+// let palpite = 0;
+
+// // Loop de repetição para continuar perguntando até acertar
+// while (palpite !== numeroSecreto) {
+//     palpite = parseInt(prompt("Tente adivinhar um número entre 1 e 100:"));
+//     tentativas++; // Incrementa o número de tentativas
+
+//     if (palpite === numeroSecreto) {
+//         alert(`Parabéns! Você acertou o número ${numeroSecreto} em ${tentativas} tentativas.`);
+//     } else if (palpite > numeroSecreto) {
+//         alert("O seu palpite é muito alto! Tente um número menor.");
+//     } else if (palpite < numeroSecreto) {
+//         alert("O seu palpite é muito baixo! Tente um número maior.");
+//     } else {
+//         alert("Por favor, digite um número válido.");
+//         tentativas--; // Não conta tentativas inválidas
+//     }
+// }
+// console.log(tentativas)
+
+
+
+//PARTE 3
+//exer 7.7
+// let i = 0
+// while(i < 250){
+//   console.log('MANHÊ, ACABEI O PRIMEIRO')
+//   console.log(i)
+//   i++
+//   //  console.log(i)
+// }
+
+// let i = 100
+// while(i > 0){
+//   i = i - 5
+//   console.log(i)
+// }
+
+//exer 7.13
+// let numero2 = 50
+// for(let numero = 1; numero < numero2; numero++){
+//   if(numero % 2 == 0){
+//     console.log(numero)
+//   }
+// }
+
+// let numero2 = 1
+// for(let numero = 50; numero > numero2; numero--){
+//   if(numero % 2 == 0){
+//     console.log(numero)
+//   }
+// }
+
+//PARTE DO EXERCICIOVERSO
+// enzo e valentina
+// let altura1 = 1.50
+// let altura2 = 1.40
+// let i 
+// for( i = 0; altura2 <= altura1; i++){
+//     altura1 += 0.02
+//     altura2 += 0.03
+// }
+// console.log(altura1)
+// console.log(altura2)
+// console.log(`${i} anos`)
+
+// let altura1 = 1.50
+// let altura2 = 1.40
+// let i = 0
+// while(altura2 <= altura1){
+//    altura1 += 0.02
+//    altura2 += 0.03 
+//    i++
+
+// }
+// console.log(altura1)
+// console.log(altura2)
+// console.log(`${i} anos`)
+
+// meia meia
+// let aumento
+// for(let i = 0; i< 3; i++){
+//   let salario = Number(prompt("digite o salário:"))
+//    aumento = salario + (salario * 10) / 100
+//    console.log(`salário: ${salario}. salário com aumento: ${aumento}`)
+// }
+
+
+//MJ E A SAGA DO XEROX
+// exer 76
+//  let s = Number(prompt('digite um número:'))
+// for(let i = 1; i <= 10; i++){
+//      let total = s*i
+//     console.log(total)
+// }
+
