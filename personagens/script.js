b8ea1fc186ce586dd1e11be42840ce6ba24620ee
JEAN TALES MENEZES DE JESUS <jean_t_jesus@estudante.sesisenai.org.br>
2026-07-07 22:20:18 -0300

    atualização

--- personagens/script.js
+++ personagens/script.js
@@ -1,34 +1,82 @@
-const personagens = ["Rivaldo Jesus","Kowalski","Genezio","Waltdisney"]
+let personagens = ["Rivaldo Jesus","Kowalski","Genezio","Waltdisney"]
+console.log(personagens)
 let posicao = []
 
+// for(let i = 0; i < personagens.length; i++){
+//      posicao.push(i)
+//     }
+//     console.log(posicao)
+
+function buscarIndice(){
+     let personagem = document.getElementById('valor').value
+     if(personagem == personagens[posicao[0]]){
+        console.log(personagem +' está no indice ' + posicao [0])
+     }else if( personagem == personagens[posicao[1]]){
+        console.log(personagem +' está no indice ' + posicao [1])
+     }else if(personagem == personagens[posicao[2]]){
+        console.log(personagem +' está no indice ' + posicao [2])
+     }else if(personagem == personagens[posicao[3]]){
+      console.log(personagem +' está no indice ' + posicao [3])
+     }else if(personagem == personagens[posicao[4]]){
+      console.log(personagem +' está no indice ' + posicao [4])
+     }
+    
+}
+
+
+function adicionar(){
+  let personagem = document.getElementById('valor').value
+   personagens.push(personagem)
+   console.log(personagens)
 
 for(let i = 0; i < personagens.length; i++){
      posicao.push(i)
     }
     console.log(posicao)
 
-function buscarIndice(){
-     let personagem = document.getElementById('valor').value
-     if(personagem == personagens[posicao[0]]){
-        console.log(personagem +' está na posição ' + posicao [0])
-     }else if( personagem == personagens[posicao[1]]){
-        console.log(personagem +' está na posição ' + posicao [1])
-     }
-    
 }
 
 
 
-//    let i = 0
-//   while(i < personagens.length){
-//     if(i == 0){
-//         console.log(`${personagens[i]} está no indice 0`)
-//     }else if(i == 1){
-//         console.log(`${personagens[i]} está no indice 1`)
-//     }else if(i == 2){
-//         console.log(`${personagens[i]} está no indice 2`)
-//     }else if(i == 3){
-//         console.log(`${personagens[i]} está no indice 3`)
+
+
+
+
+
+
+
+
+
+
+
+   
+// let personagem
+// function adicionar(){
+//    personagem = document.getElementById('valor').value
+   
+//    for( i = 0; i < personagens.length; i++){
+//      personagens[i]
+    
+//     if(personagem != personagens[i]){
+//       personagens.push(personagem)
 //     }
-//     i++
+   
+//     }
+//    personagens.push(personagem)
+//    console.log(personagens)
+
+// }
+
+// function buscarIndice(){
+//    let i
+//    for( i = 0; i < personagens.length; i++){
+//      personagens[i]
+    
+//     if(personagem == personagens[i]){
+//       console.log(`${personagens[i]} está no indice ${i}`)
+//     }
+   
+   
+//     }
+    
 // }
